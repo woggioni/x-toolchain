fccda8e2cbcdda8152016c52165f90f958c0f7af
Walter Oggioni <oggioni.walter@gmail.com>
2023-10-14 15:49:45 +0800

    fixed rpath and dynamic linker path for generated executables

--- packages/configure/x-configure.sh
+++ packages/configure/x-configure.sh
@@ -9,7 +9,7 @@ else
   config_path=".."
 fi
 
-LDFLAGS=""
+export LDFLAGS="-Wl,-rpath=@PREFIX@/@TRIPLE@/sysroot/lib:@PREFIX@/@TRIPLE@/sysroot/usr/lib -Wl,-dynamic-linker=@DYNAMIC_LINKER@"
 ${config_path}/configure --build="$CHOST" --host=@TRIPLE@ --target=@TRIPLE@ \
   --prefix=@PREFIX@/@TRIPLE@/sysroot \
   --sbindir=@PREFIX@/@TRIPLE@/sysroot/usr/sbin \
