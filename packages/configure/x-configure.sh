da248994b2ba1c870e3e899b85a183487e1ced8e
Walter Oggioni <oggioni.walter@gmail.com>
2024-05-25 17:36:47 +0800

    added arm 32 bit roolchain

--- packages/configure/x-configure.sh
+++ packages/configure/x-configure.sh
@@ -8,9 +8,8 @@ then
 else
   config_path=".."
 fi
-
 export LDFLAGS="-Wl,--as-needed -Wl,-rpath=@PREFIX@/@TRIPLE@/sysroot/lib:@PREFIX@/@TRIPLE@/sysroot/usr/lib -Wl,-dynamic-linker=@DYNAMIC_LINKER@"
-exec ${config_path}/configure --build="$CHOST" --host=@TRIPLE@ --target=@TRIPLE@ \
+exec ${config_path}/configure "--build=@BUILD_TRIPLE@" "--host=@TRIPLE@" "--target=@TRIPLE@" \
   --prefix=@PREFIX@/@TRIPLE@/sysroot \
   --sbindir=@PREFIX@/@TRIPLE@/sysroot/usr/sbin \
   --bindir=@PREFIX@/@TRIPLE@/sysroot/usr/bin \
